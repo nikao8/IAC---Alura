a01833a8c4d1c8e2fa67f3cfda25965c9588b2d9
nikao8 <nicolasmarquesjf@hotmail.com>
2025-07-29 20:46:09 -0300

    Update security group ingress rule to restrict HTTP access to specified IP

--- Curso1 /main.tf
+++ Curso1 /main.tf
@@ -34,7 +34,7 @@ resource "aws_security_group" "ssh_sg" {
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks      = [var.my_ip]
   }
 
   egress {
