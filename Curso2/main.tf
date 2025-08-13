295bb3efd3da9bc1f581b4c78b24b9909b9c9eb7
nikao8 <nicolasmarquesjf@hotmail.com>
2025-08-12 21:58:13 -0300

    Add MySQL configuration and update provisioning for WordPress setup

--- Curso2/main.tf
+++ Curso2/main.tf
@@ -17,9 +17,9 @@ variable "my_ip" {
 }
 
 ########### Extra passando criacao do SecurityGroup e adicionando regra de entrada para ssh no meuip ###########
-resource "aws_security_group" "access_sg" {
-  name        = "server-access-sg"
-  description = "Allow SSH access from my IP"
+resource "aws_security_group" "access_wordpress" {
+  name        = "access-wordpress-sg"
+  description = "Allow access to WordPress"
   vpc_id      = data.aws_vpc.default.id
 
   ingress {
@@ -47,6 +47,29 @@ resource "aws_security_group" "access_sg" {
   }
 }
 
+
+resource "aws_security_group" "access_mysql" {
+  name        = "access-mysql-sg"
+  description = "Allow access to Wordpress MySQL"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description                = "MYSQL access from WordPress"
+    from_port                  = 3306
+    to_port                    = 3306
+    protocol                   = "tcp"
+    security_groups            = [aws_security_group.access_wordpress.id]  # aqui liberamos atraves do security group do wordpress, ou seja, quem estiver naquele sg sera liberado neste
+  }
+
+  egress {
+    description      = "Allow all outbound traffic"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
 # Obter VPC padrão (necessário para associar o SG)
 data "aws_vpc" "default" {
   default = true
@@ -56,9 +79,20 @@ resource "aws_instance" "ec2_instance_wordpress" {
   ami                    = "ami-020cba7c55df1f615" // ubuntu t3.micro ami
   instance_type          = "t3.micro"
   key_name               = "iac-nicolas"
-  vpc_security_group_ids = [aws_security_group.access_sg.id]
+  vpc_security_group_ids = [aws_security_group.access_wordpress.id]
  
   tags = {
-    Name = "Curso Ansible"
+    Name = "Curso Ansible - Wordpress"
+  }
+}
+
+resource "aws_instance" "ec2_instance_mysql" {
+  ami                    = "ami-020cba7c55df1f615" // ubuntu t3.micro ami
+  instance_type          = "t3.micro"
+  key_name               = "iac-nicolas"
+  vpc_security_group_ids = [aws_security_group.access_mysql.id]
+ 
+  tags = {
+    Name = "Curso Ansible - Mysql"
   }
 }
